72e51cd17a1faec6ec8ed5d69fd36f54319cfa86
mamtatpednekar <73109319+mamtatpednekar@users.noreply.github.com>
2022-04-04 23:20:18 -0700

    Add files via upload
    Write a method to replace all spaces in a string with "%20".

---
+++ ReplaceSpacewithValue.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def convertSpacesToValue(string):
+    newString = string.replace(' ', '%20')
+    print(newString)
+    
+convertSpacesToValue("Hi How are you")
+
+
+# In[ ]:
+
+
+
+
